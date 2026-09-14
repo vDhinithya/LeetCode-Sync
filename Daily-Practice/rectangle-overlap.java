073f0f3b221c2527a7ed07dc2c37c8fd2b7f0758
vDhinithya <dhinithya12@gmail.com>
2026-09-14 16:42:18 +0000

    Auto-sync latest LeetCode solutions

---
+++ Daily-Practice/rectangle-overlap.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        boolean xOverlap = Math.min(rec1[2], rec2[2]) > Math.max(rec1[0], rec2[0]);
+        boolean yOverlap = Math.min(rec1[3], rec2[3]) > Math.max(rec1[1], rec2[1]);
+        
+        return xOverlap && yOverlap;
+    }
+}
